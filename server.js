6e71f6a5eca0e4e4b0052572e04d03f87ebfda9f
bettercodevivek <viveksingh0520@gmail.com>
2026-01-04 19:47:45 +0530

    CORS fix test

--- server.js
+++ server.js
@@ -21,15 +21,7 @@ const PORT = process.env.PORT || 5000;
 
 /* -------------------- MIDDLEWARE -------------------- */
 
-app.use(
-  cors({
-    origin: [
-      process.env.FRONTEND_URL,
-      "http://localhost:5173",
-    ],
-    credentials: true,
-  })
-);
+app.use(cors({ origin: true, credentials: true }));
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
